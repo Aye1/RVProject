0389efec8c6123f0fb62d7879ab58cf456591cb2
Laura Peythieux <laura@Kenshin.(none)>
2011-01-26 23:54:46 +0100

    bis

---
+++ CommonReal/wiim.h
@@ -0,0 +1,59 @@
+/*
+ * WARNING : pour que le code compile, il est nécessaire de taper la commande
+ * export QMAKEFEATURES=~/RVProject/api_arv/qmake
+ * (éventuellement, l'ajouter dans le .bashrc ou équivalent)
+ */
+
+#ifndef WIIM_H_
+#define WIIM_H_
+
+#include <ApiArv/Wii.h>
+
+using namespace arv;
+using namespace std;
+
+class Wiim { 
+
+protected:
+
+	Wii* _wii;
+	bool _stop;
+	cwiid_ir_src* _wiimote1;
+	cwiid_ir_src* _wiimote2;
+	float acc1;
+	float acc2;
+	int wiiZone1;
+	int wiiZone2;
+	bool valid1;
+	bool valid2;
+
+
+public:
+
+	Wiim();
+	
+	arv::Wii* getWii();
+
+	bool getStop();
+	
+	int getWiiZone1();
+	int getWiiZone2();
+
+	bool getValid1();
+	bool getValid2();
+
+	float getAcc1();
+	float getAcc2();
+	
+	void init();
+
+	void getPos(float& pos1x, float& pos2x, float& pos1y, float& pos2y);
+
+	void getVal();
+
+	void close();
+
+}
+
+#endif
+
