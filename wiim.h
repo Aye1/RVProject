f0c40b3fccaa1700ad97bb267f5aed87298a5a26
Emeline Bantegnie <bantegne@arv07.(none)>
2011-01-26 14:32:06 +0100

    voilà, tu peux t'amuser à faire du code propre maintenant, wiim.h c'est là où ya toutes les fonctions, getVal c'est pour récupérer si ya un truc qui est validé, getPos c'est pour récupérer les positions  des deux wiimotes (pour l'affichage des baguettes), et wiimote.cpp c'est juste un main pour tester si ça marche

--- wiim.h
+++ wiim.h
@@ -5,99 +5,84 @@
 #include <csignal>
 #include <stdio.h>
 #include <stdlib.h>
-#include "wiim.h"
+
 
 using namespace arv;
 using namespace std;
 
 
+bool stop = false;
 
-
-int main(int /*argc*/, char* /*argv*/[]) {
-
-	Wii *wii = NULL;
-	wii = init();
-	
-	int wiiZone1 = 0;
-	int wiiZone2 = 0;
-	bool valid1 = false;
-	bool valid2 = false;
-	float acc1 = 0;
-	float acc2 = 0;
-	float pos1x, pos2x, pos1y, pos2y;
-	int i = 0;
-	while (!stop) {
-		getVal(wii, wiiZone1, wiiZone2, valid1, valid2, acc1, acc2);
-		if (valid1) {
-			cout << "wii1 validee dans la zone " << wiiZone1 << endl;
-		}
-		if (valid2) {
-			cout << "wii2 validee dans la zone " << wiiZone2 << endl;
-		}
-		getPos(wii, pos1x, pos2x, pos1y, pos2y);
-		if (i < 100000) {
-			i++;
-		} else {
-			cout << "position 1 " << pos1x << " " << pos1y << " position 2 " << pos2x << " " << pos2y << endl;
-			i = 0;
-		}
-	}
-	close(wii);
-	return 0;
+void sigproc(int /*i*/) {
+    stop = true;
 }
-/*
-int init(Wii& wii) {
+
+//int main(int /*argc*/, char* /*argv*/[]) {
+    
+Wii* init() {
 	signal(SIGINT, sigproc);
 	signal(SIGQUIT, sigproc);
 
 	stop = false;
 	cout << "Connexion des wiimotes" << endl;
-	wii = new Wii();
+	Wii* wii = new Wii();
 	cout << "> Wii allocated." << endl;
 
 	cout << "> Adding the first wiimote" << endl;
 	wii->addWiimote();
-	cwiid_ir_src* wii1;
-	cwiid_ir_src* wii2;
+	//cwiid_ir_src* wii1;
+	//cwiid_ir_src* wii2;
 
 	cout << "> Adding the second wiimote" << endl;
 	wii->addWiimote();
-	return 1;
+	return wii;
 }
 
-int getPos(Wii& wii, int& wiiZone1, int& wiiZone2, bool& valid1, bool& valid2) {
+void getPos(Wii *wii, float& pos1x, float& pos2x, float& pos1y, float& pos2y) {
+	cwiid_ir_src* wii1;
+	cwiid_ir_src* wii2;
+	double a1x, a1y, a1z, a2x, a2y, a2z;
+	wii1 = wii->getIRData(0);
+	wii2 = wii->getIRData(1);
+	pos1x = wii1->pos[0];
+	pos2x = wii2->pos[0];
+	pos1y = wii1->pos[1];
+	pos2y = wii2->pos[1];
+}	
+ 
+void getVal(Wii *wii, int& wiiZone1, int& wiiZone2, bool& valid1, bool& valid2, float& acc1, float& acc2) {
  
 	// juste pour vérifier que ça compile
-	int taillecaisse4pieds = 150;	
+	/*int taillecaisse4pieds = 150;	
 	int taillecaisse3pieds = 150;
 	int taillecaisse2pieds = 150;
 	int taillecaisse1pieds = 150;
 	int taillepieds4 = 50;
 	int taillepieds3 = 50;
 	int taillepieds2 = 50;
-	int taillepieds1 = 50;
+	int taillepieds1 = 50;*/
 	cwiid_ir_src* wii1;
 	cwiid_ir_src* wii2;
-	wiiZone1 = 0;
-	wiiZone2 = 0;
+//	wiiZone1 = 0;
+//	wiiZone2 = 0;
 	double a1x, a1y, a1z, a2x, a2y, a2z;
 	bool rumble = false;
 	valid1 = false;
 	valid2 = false;
 //	while(!stop) {
-		if (wii->update()){
-			wii1 = wii->getIRData(0);
-			wii2 = wii->getIRData(1);
-			wii->getAcceleration(a1x, a1y, a1z, 0); 
-			wii->getAcceleration(a2x, a2y, a2z, 1);
+	if (wii->update()) {
+		wii1 = wii->getIRData(0);
+		wii2 = wii->getIRData(1);
+		wii->getAcceleration(a1x, a1y, a1z, 0); 
+		wii->getAcceleration(a2x, a2y, a2z, 1);
 		if (a1y > 1.5 || a2y > 1.5 ) {
 			//blabla validate touche
-			if (a1y > 1.5) {
-				cout << "wii1 " << wiiZone1 << endl;
+			if (a1y > 1.5 && acc1 < 1.5) {
+		//		cout << "wii1 " << wiiZone1 << endl;
 				valid1 = true;
 			}
-			if (a2y > 1.5) {
-				cout << "wii2 " << wiiZone2 << endl;
+			if (a2y > 1.5 && acc2 < 1.5) {
+		//		cout << "wii2 " << wiiZone2 << endl;
 				valid2 = true;
 			}
 	    	} else {
@@ -156,6 +141,8 @@ int getPos(Wii& wii, int& wiiZone1, int& wiiZone2, bool& valid1, bool& valid2) {
                	 		}
         	    	}
             	}
+		acc1 = a1y;
+		acc2 = a2y;
 	
             int buttons = wii->getButtonPressed(0);
             if (buttons & CWIID_BTN_A) {
@@ -169,12 +156,9 @@ int getPos(Wii& wii, int& wiiZone1, int& wiiZone2, bool& valid1, bool& valid2) {
         }
     //}
 
-	return 1;
 }
 
-int close(Wii& wii) {
+void close(Wii *wii) {
     wii->close();
     cout << "> device closed." << endl;
-
-    return 0;
-}*/
+}
