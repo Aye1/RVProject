752a3fa949210f6a8f911ed571dae0f36d4b7394
Adrien Janvier <janvier.adrien@gmail.com>
2011-01-27 11:59:50 +0100

    ajout des touches en fonction de l'int du fichier

--- CommonReal/AGHListeTouches.cpp
+++ CommonReal/AGHListeTouches.cpp
@@ -4,6 +4,27 @@ AGHListeTouches::AGHListeTouches() {
 }
 
 void AGHListeTouches::addNotes(int notes) {
+	Touche * tmp;
+	if (notes & 2) {
+		tmp = new Touche();
+		tmp->setColor(0.0f, 1.0f, 0.0f);
+		this->append(tmp);
+	}
+	if (notes & 4) {
+		tmp = new Touche();
+		tmp->setColor(1.0f, 0.0f, 0.0f);
+		this->append(tmp);
+	}
+	if (notes & 8) {
+		tmp = new Touche();
+		tmp->setColor(1.0f, 1.0f, 0.0f);
+		this->append(tmp);
+	}
+	if (notes & 16) {
+		tmp = new Touche();
+		tmp->setColor(0.0f, 0.0f, 1.0f);
+		this->append(tmp);
+	}
 }
 
 void AGHListeTouches::updateNotesPos() {
