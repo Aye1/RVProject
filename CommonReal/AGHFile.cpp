8ad339f969e2bada7929a266cde9b169c39678a8
Adrien Janvier <janvier.adrien@gmail.com>
2011-01-27 14:20:36 +0100

    test

--- CommonReal/AGHFile.cpp
+++ CommonReal/AGHFile.cpp
@@ -14,6 +14,7 @@ int AGHFile::nextNote() {
 		return -1;
 	}
 	QByteArray line = this->readLine();
+	std::cout << "taille : " << line.size() << endl;
 	char number = line.at(0);
 	return atoi(&number);
 }
